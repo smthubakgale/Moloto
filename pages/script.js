f4dca1709eb6fc2db4f077831f5ff10503000823
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2024-09-06 07:56:26 +0200

    Update script.js

--- pages/script.js
+++ pages/script.js
@@ -5,7 +5,7 @@ function createModel() {
   const model = tf.sequential();
 
   // Add a single input layer
-  model.add(tf.layers.dense({inputShape: [1 , 1], units: 1, useBias: true}));
+  model.add(tf.layers.dense({inputShape: [1 , 1], units: 50, useBias: true}));
 
   // Add an output layer
   model.add(tf.layers.dense({units: 1, useBias: true}));
