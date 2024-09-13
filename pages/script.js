64593e02a91a3ef453d26d478d9e2b0ed699da09
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2024-09-13 15:30:53 +0200

    Update script.js

--- pages/script.js
+++ pages/script.js
@@ -71,7 +71,7 @@ function createModel()
 {
   var model = tf.sequential({
        layers: [
-         tf.layers.dense({inputShape: [2,40], units:1 , activation: 'relu'}),    // input size [n ,2 ,40] -> shape [2,40] => [2 , 40*1] => [batch , 2 , 40]
+         tf.layers.dense({inputShape: [2,40], units:32 , activation: 'relu'}),    // input size [n ,2 ,40] -> shape [2,40] => [2 , 40*1] => [batch , 2 , 40]
          tf.layers.flatten() ,                                                   // flatten -> [batch , 2*40 ] => [ batch , 80] 
          tf.layers.dense({inputshape:[40] , units:1 ,  activation: 'softmax'}),   // output shape [n, 40 ] -> shape [40] => [40*1] => [batch , 40]
        ]
