a191b75ee1538872d8a5a93a841aa4d4f1ba0295
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2024-09-20 15:24:16 +0200

    Update script.js

--- pages/script.js
+++ pages/script.js
@@ -53,25 +53,25 @@ function saveModel(cb)
     const LEARNING_RATE = 0.25;
     const optimizer = tf.train.sgd(LEARNING_RATE);
     let item = Number(localStorage.getItem('saveNo'));
-
-       if(item > 0)
-       { 
-            tf.loadModel(`indexeddb://hevoPredic-${item}`).then((md)=>
-            {
-                var model = md;
-                
-                model.compile({
-                  optimizer: optimizer,
-                  loss: 'categoricalCrossentropy',
-                  metrics: ['accuracy'],
-                });
-        
-                cb(model);
+    console.log(item);
+   if(item > 0)
+   { 
+        tf.loadModel(`indexeddb://hevoPredic-${item}`).then((md)=>
+        {
+            var model = md;
+            
+            model.compile({
+              optimizer: optimizer,
+              loss: 'categoricalCrossentropy',
+              metrics: ['accuracy'],
             });
-       }
-       else{
-           cb(null);
-       }
+    
+            cb(model);
+        });
+   }
+   else{
+       cb(null);
+   }
   } 
   else 
   {
